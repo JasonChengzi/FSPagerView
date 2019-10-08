d93bf78406d09ac20c0fe941f59b5f9f83f7c61c
JasonLee@work <liweijie@dabaikeji.cn>
2019-10-08 11:23:07 +0800

    Update 更新Cell的阴影，适配项目UI设计；

--- Sources/FSPagerViewCell.swift
+++ Sources/FSPagerViewCell.swift
@@ -102,8 +102,8 @@ open class FSPagerViewCell: UICollectionViewCell {
         self.contentView.backgroundColor = UIColor.clear
         self.backgroundColor = UIColor.clear
         self.contentView.layer.shadowColor = UIColor.black.cgColor
-        self.contentView.layer.shadowRadius = 5
-        self.contentView.layer.shadowOpacity = 0.75
+        self.contentView.layer.shadowRadius = 6
+        self.contentView.layer.shadowOpacity = 0.10
         self.contentView.layer.shadowOffset = .zero
     }
     
